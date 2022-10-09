b0eed91422b6ef29be8be779cc3dd21ae99ea0c6
Ben <benawhiting@gmail.com>
2022-10-09 13:40:37 -0700

    Create stopwatch component for measuring time taken

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
+
+import Stopwatch from './Stopwatch';
 import '../stylesheets/Header.css';
 
 const Header = () => {
   return (
     <div className='header-container'>
       <div className="home-btn">Where's Waldo</div>
-      <div className="timer">Timer Goes Here</div>
+      <Stopwatch />
       <div className="remaining">Characters Remaining</div>
     </div>
   );
---
+++ src/components/Stopwatch.js
@@ -0,0 +1,39 @@
+// Stopwatch component for measuring time taken to finish the game
+
+import React, { useState, useEffect } from 'react';
+
+// SINCE NOT USING BUTTONS, WILL NEED TO UPDATE TIMER STATUS BY PASSING `RUNNING` STATE MANUALLY
+const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(true);
+
+  useEffect(() => {
+    let interval;
+
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [running]);
+
+  return (
+    <div className="stopwatch">
+      <div className="numbers">
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span>
+      </div>
+      {/* <div className="buttons">
+        <button onClick={() => setRunning(true)}>Start</button>
+        <button onClick={() => setRunning(false)}>Stop</button>
+        <button onClick={() => setTime(0)}>Reset</button>       
+      </div> */}
+    </div>
+  )
+};
+
+export default Stopwatch;
