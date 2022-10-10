a700377b88ca8130567c187039e23f7ae1e0096f
Ben <benawhiting@gmail.com>
2022-10-10 13:31:43 -0700

    Create basic dropdown

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Dropdown from './Dropdown';
 import Waldo from '../images/waldo1.jpg';
 import '../stylesheets/Body.css';
 
 const Body = () => {
   return (
     <div className='body-container'>
+      <Dropdown />
       <img src={Waldo} alt='Wheres Waldo' className='game' />
     </div>
   );
---
+++ src/components/Dropdown.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../stylesheets/Dropdown.css';
+
+const Dropdown = () => {
+  return (
+    <div className='dropdown'>
+      <ul>
+        <li>Waldo</li>
+        <li>Odlaw</li>
+        <li>Wizard</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Dropdown;
