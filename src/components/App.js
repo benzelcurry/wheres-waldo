bf1c1d827e2e3af7582b68353567ce39f50a40d5
Ben <benawhiting@gmail.com>
2022-10-11 12:24:19 -0700

    Hide dropdown on page refresh and when clicked off of

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from './Header';
 import Body from './Body';
-import Dropdown from './Dropdown';
 
 // RUN `NPM RUN BUILD` TO PUSH CHANGES TO FIREBASE
 const App = () => {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-
-  // Make so it doesn't go off the screen when on borders
-  const changeCoords = (e) => {
-    const clickLeft = e.pageX;
-    const clickTop = e.pageY;
-
-    setX(clickLeft);
-    setY(clickTop);
-
-    console.log(x, y);
-  };
-
   return (
-    <div onClick={(e) => changeCoords(e)}>
+    <div >
       <Header />
       <Body />
-      <Dropdown newX={x} newY={y} />
     </div>
   );
 };
--- src/components/Body.js
+++ src/components/Body.js
@@ -5,26 +5,30 @@ import Waldo from '../images/waldo1.jpg';
 import '../stylesheets/Body.css';
 
 const Body = () => {
-  // const [x, setX] = useState(0);
-  // const [y, setY] = useState(0);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [drop, setDrop] = useState(false);
 
-  // const changeCoords = (e) => {
-  //   const clickLeft = e.clientX;
-  //   const clickTop = e.clientY;
+  const changeCoords = (e) => {
+    const clickLeft = e.pageX;
+    const clickTop = e.pageY;
 
-  //   setX(clickLeft);
-  //   setY(clickTop);
+    setX(clickLeft);
+    setY(clickTop);
 
-  //   console.log(x, y);
-  // };
+    drop ? setDrop(false) : setDrop(true);
+  };
 
   // REFINE LOGIC THAT SHOWS WHERE DROPDOWN APPEARS ON SCREEN WHEN CLICKING (IT GETS WEIRD WHEN YOU SCROLL); MAKE APPEAR TO RIGHT OF CURSOR
   // MAKE DROPDOWN HIDDEN BY DEFAULT
   // MAKE DROPDOWN DISAPPEAR WHEN CLICKED OUTSIDE OF
   // ADD CIRCLE SHOWING AREA WHERE USER IS CLICKING ON IN ADDITION TO THE DROPDOWN
   return (
-    <div className='body-container'>
-      {/* <Dropdown newX={x} newY={y} /> */}
+    <div className='body-container' onClick={(e) => changeCoords(e)}>
+      { drop ? 
+        <Dropdown newX={x} newY={y} />
+        : null
+      }
       <img src={Waldo} alt='Wheres Waldo' className='game' />
     </div>
   );
