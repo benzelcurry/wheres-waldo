c1ccc037cc2f41abb43b1f552f7c014edc8a6834
Ben <benawhiting@gmail.com>
2022-10-16 12:59:04 -0700

    Make leaderboard pull data from Firestore

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -8,7 +8,11 @@ const Leaderboard = ({ db, collection, getDocs }) => {
   // Include ', []' after trailing '}'?
   useEffect(() => {
     getScores()
-  });
+  }, []);
+
+  useEffect(() => {
+    console.log(hiscores);
+  }, [hiscores]);
 
   // COME BACK AND FINISH THIS; HOPEFULLY FIXES INFINITE LOOPING
   const getScores = () => {
@@ -16,13 +20,25 @@ const Leaderboard = ({ db, collection, getDocs }) => {
 
     getDocs(scoresColRef)
       .then(response => {
-        console.log(response);
+        const scores = response.docs.map(doc => ({ 
+          data: doc.data(), id: 
+          doc.id 
+        }))
+        setHiscores(scores);
       })
       .catch(err => console.log(err.message));
   }
 
   return (
     <div className='leaderboard-container'>
+      <div className='leaderboard-title'>Leaderboard</div>
+      <ol className='score-container'>
+        { hiscores.map(score => 
+          <li key={score.id} className='score'>
+            <div>{ score.data.name }</div>
+            <div>{ score.data.score }</div>
+          </li>) }
+      </ol>
     </div>
   )
 };
